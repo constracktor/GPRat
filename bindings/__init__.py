9d239bb13f52939638fd0c910fd41fc13538beba
constracktor <strack.as@icloud.com>
2025-02-17 15:03:49 +0100

    Switch to scikit-build-core

---
+++ bindings/__init__.py
@@ -0,0 +1,5 @@
+# import all bindings from the compiled GPRat module
+from .gprat import *
+# explicitly set the module level attributes
+#__doc__ = gprat.__doc__
+#__version__ = gprat.__version__
--- create_python_package.sh
+++ create_python_package.sh
@@ -2,37 +2,34 @@
 
 # Create & Activate python environment
 if [ ! -d "pypi_env" ]; then
-    python3 -m venv pypi_env
+    python -m venv pypi_env
 fi
 source pypi_env/bin/activate
 
 # Install requirements
-pip3 install --upgrade pip
-pip3 install setuptools 
-pip3 install twine
-pip3 install --upgrade wheel
-pip3 install --upgrade packaging
+pip install --upgrade pip
+pip install twine
 
 # Load required libs
 spack load gcc@14.2.0
+export CXX=$(which g++)
 spack load cmake
 spack env activate gprat_cpu_gcc
+
+# Test: Manually install GPRat to pip
+pip uninstall -y gprat
+pip install .
+# Test: Check if import works
+python -c "import gprat"
+# Test: Run gprat_python example
+cd examples/gprat_python
+python execute.py
+cd ../..
 
 # Build package 
 # This command will generate distribution archives 
 # (.tar.gz and .whl) in the dist directory
-python3 setup.py sdist bdist_wheel
-rm -rf build
-
-# Test: Manually install GPRat to pip
-pip3 uninstall -y gprat
-pip3 install .
-# Test: Check if import works
-python3 -c "import gprat"
-# Test: Run gprat_python example
-cd examples/gprat_python
-python3 execute.py
-cd ../..
-
+#python3 setup.py sdist bdist_wheel
+#rm -rf build
 # Upload to PyPI
-python3 -m twine upload dist/*
+#python -m twine upload dist/*
