209ab2220706619a8eedc0f73424bf403bcc0831
constracktor <strack.as@icloud.com>
2025-06-03 14:47:59 +0200

    Ensure coexistence of gcc and clang

--- examples/gprat_cpp/run_gprat_cpp.sh
+++ examples/gprat_cpp/run_gprat_cpp.sh
@@ -9,9 +9,9 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 if [[ -z "$1" ]]; then
     echo "Input parameter is missing. Using default: Run computations on CPU"
-elif [[ "$2" == "gpu" ]]; then
+elif [[ "$1" == "gpu" ]]; then
     use_gpu="--use_gpu"
-elif [[ "$2" != "cpu" ]]; then
+elif [[ "$1" != "cpu" ]]; then
     echo "Please specify input parameter: cpu/gpu"
     exit 1
 fi
@@ -19,22 +19,32 @@ fi
 if command -v spack &> /dev/null; then
     echo "Spack command found, checking for environments..."
 
-    # Check if the gprat_cpu_gcc environment exists
-    if spack env list | grep -q "gprat_cpu_gcc"; then
-        echo "Found gprat_cpu_gcc environment, activating it."
-        module load gcc/14.1.0
-        spack env activate gprat_cpu_gcc
-        GPRAT_WITH_CUDA=OFF # whether GPRAT_WITH_CUDA is ON of OFF is irrelevant for this example
+    # Get current hostname
+    HOSTNAME=$(hostname)
 
-    # Check if the gprat_gpu_clang environment exists
-    elif spack env list | grep -q "gprat_gpu_clang"; then
-        echo "Found gprat_gpu_clang environment, activating it."
-        module load clang/17.0.1
-        module load cuda/12.0.1
-        spack env activate gprat_gpu_clang
-        GPRAT_WITH_CUDA=ON
+    if [[ "$HOSTNAME" == "ipvs-epyc1" ]]; then
+	# Check if the gprat_cpu_gcc environment exists
+    	if spack env list | grep -q "gprat_cpu_gcc"; then
+	   echo "Found gprat_cpu_gcc environment, activating it."
+	    module load gcc/14.2.0
+	    export CXX=g++
+	    export CC=gcc
+	    spack env activate gprat_cpu_gcc
+	    GPRAT_WITH_CUDA=OFF # whether GPRAT_WITH_CUDA is ON of OFF is irrelevant for this example
+	fi
+    elif [[ "$HOSTNAME" == "simcl1n1" || "$HOSTNAME" == "simcl1n1" ]]; then
+	# Check if the gprat_gpu_clang environment exists
+	if spack env list | grep -q "gprat_gpu_clang"; then
+	    echo "Found gprat_gpu_clang environment, activating it."
+	    module load clang/17.0.1
+	    export CXX=clang++
+	    export CC=clang
+	    module load cuda/12.0.1
+	    spack env activate gprat_gpu_clang
+	    GPRAT_WITH_CUDA=ON
+	fi
     else
-        echo "Neither gprat_cpu_gcc nor gprat_gpu_clang environments found. Building example without Spack."
+    	echo "Hostname is $HOSTNAME — no action taken."
     fi
 else
     echo "Spack command not found. Building example without Spack."
